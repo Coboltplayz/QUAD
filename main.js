90597b1886aca6324b50939f5535e40d38881040
Coboltplayz <90228566+Coboltplayz@users.noreply.github.com>
2022-01-06 14:32:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function getStory(name) {
 return { 
 currentScene: "beginning", /*This is always the current scene. It changes when you click or choose choices.*/
 beginning: { /*Events: pg1, pg2, and beginning*/
-title: "Quack", 
+title: "Quack!            (First Chapter and the quarter of the game)", 
 story: `You woke in the night and you stare at the clock, it was 3:AM.`,
 image: "", 
 choices: [
@@ -13,14 +13,14 @@ choices: [
 	destination: 'pg1'
 	},
 	{
-	choice: "Go to Bed", 
+	choice: "Go back to Bed", 
 	destination: 'pg2'
 	}
 ]
 },
 pg1: { /*From: beginning | To: pg3, pg4, pg5*/
 title: "The Kitchen", 
-story:`You went to the kitchen. You were hungry. So what will u eat?`,
+story:`You went to the kitchen. You were hungry. So what will you eat?`,
 image: "", 
 choices: [
 	{
@@ -39,7 +39,7 @@ choices: [
 },
 pg2: { /*From: beginning | To: pg4 and pg7*/
 title: "Going to bed", 
-story:`You go to bed but you saw a shadow. What will u do?`,
+story:`You go back to bed but you saw a shadow. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -54,7 +54,7 @@ choices: [
 },
 pg3: { /*From: pg1 | To: pg8 and pg7*/
 title: "Eating the Burger", 
-story:`You are gonna eat the burger so u pick it up and eat it but you saw a shadow. What will u do?`,
+story:`You are gonna eat the burger so you pick it up and eat it but you saw a shadow. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -69,7 +69,7 @@ choices: [
 },
 pg4: { /*From: pg1 and pg2 | To: pg7 and pg2*/
 title: "Eating nothing", 
-story:`You ate nothing and saw a shadow. What will u do?`,
+story:`You ate nothing and saw a shadow. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -84,7 +84,7 @@ choices: [
 },
 pg5: { /*From: pg1 | To: pg7 and pg8*/
 title: "Eating the apple", 
-story:`You are gonna eat the apple and saw a shadow. What will u do?`,
+story:`You are gonna eat the apple and saw a shadow. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -99,7 +99,7 @@ choices: [
 },
 pg6: { /*From: pg8 | To: pg9 and pg7*/
 title: "Going to the bed", 
-story:`You ignored the shadow and go to bed.`,
+story:`You ignored the shadow and went to bed.`,
 image: "", 
 choices: [
 	{
@@ -114,7 +114,7 @@ choices: [
 },
 pg7: { /*From: pg2, pg3, pg4, pg5, pg6, and pg8| To: pg10 and pg11*/
 title: "Knowing the Shadow", 
-story:`You think what was the shadow. The shadow appeared in front of you. What will u do?`,
+story:`You think what was the shadow. The shadow appeared in front of you. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -133,7 +133,7 @@ story:`You ignored the shadow and ate the burger.`,
 image: "", 
 choices: [
 	{
-	choice: "Go to bed",
+	choice: "Go back to bed",
 	destination: 'pg6'
 	},
 	{
@@ -148,7 +148,7 @@ story:`You ignored the shadow and ate the apple.`,
 image: "", 
 choices: [
 	{
-	choice: "Go to bed",
+	choice: "Go back to bed",
 	destination: 'pg6'
 	},
 	{
@@ -159,21 +159,21 @@ choices: [
 },
 pg9: { /*From: pg6| To: end*/
 title: "Sleeping", 
-story:`ZZZZZZZZZZZzzzzzzzz`,
+story:`ZZZZZZZZZZZZZZZZZZZZZZzzzzzzzzzzzzzzzzzzzzzzzzzzz_`,
 image: "", 
 defaultDestination: "end",
 buttonText: 'Next'
 },
 end: { /*From: anyDeath | To: beginning*/
-title: "GAME OVER", 
-story:`You died, the end.`,
+title: "GAME OVER!", 
+story:`You died ${name}, the end...`,
 image: "", 
 defaultDestination: "beginning",
-buttonText: 'Restart?'
+buttonText: 'Restart I Guess?'
 },
 pg10: { /*From: pg7  | To: pg 12 and pg13*/
 title: "Running", 
-story:`U run and want to go outside your house. But there was a door that you lock every night. So what will you do ${name}?`,
+story:`You run and want to go outside your house. But there was a door that you lock every night. So what will you do ${name}?`,
 image: "", 
 choices: [
 	{
@@ -214,7 +214,7 @@ choices: [
 },
 pg13: { /*From: pg10 and pg12 | To: pg14 and end*/
 title: "Kicking the door", 
-story:`You kicked the door but it won't bugde open.`,
+story:`You kicked the door but it won't budge open.`,
 image: "", 
 choices: [
 	{
@@ -229,7 +229,7 @@ choices: [
 },
 pg14: { /*From: pg13 | To: pg15 and pg16 */
 title: "Kicking the door again", 
-story:'You try kicking the door again and it open! So where will u go?',
+story:'You try kicking the door again and it open! So where will you go?',
 image: "", 
 choices: [
 	{
@@ -237,14 +237,14 @@ choices: [
 	destination: 'pg15'
 	},
 	{
-	choice: "Go to other house", 
+	choice: "Go to the other houses", 
 	destination: 'pg16'
 	}
 ]
 },
 pg15: { /*From: pg14 | To: pg17 and pg16 */
 title: "Going to your car", 
-story:`You go to your car and open the door with the keys. You go inside and saw the shadow following you. What will u do?`,
+story:`You go to your car and open the door with the keys. You go inside and saw the shadow following you. What will you do?`,
 image: "", 
 choices: [
 	{
@@ -377,8 +377,8 @@ choices: [
 ]
 },
 pg22: { /*From: pg18 | To: end*/
-title: "Going inside of Kevins House", 
-story:`You are going inside of Kevins house but the shadow appeared in front of you.`,
+title: "Going inside of Kevin's House", 
+story:`You are going inside of Kevin's house but the shadow appeared in front of you.`,
 image: "", 
 choices: [
 	{
@@ -389,7 +389,7 @@ choices: [
 },
 pg23: { /*From: pg18 | To: pg17mod1 and pg21mod1*/
 title: "You run", 
-story:`You run. Where will u go?`,
+story:`You run. Where will you go?`,
 image: "", 
 choices: [
 	{
@@ -397,14 +397,14 @@ choices: [
 	destination: 'pg20mod1'
 	},
 	{
-	choice: "Go to police station", 
+	choice: "Go to the police station", 
 	destination: 'pg17mod1'
 	}
 ]
 },
 pg24: { /*From: pg19 | To: pg27 and pg28*/
 title: "Talking to Sams family", 
-story:`You walk to Sams family. "Are you okay ${name}? Sam was gone when we woke up. DO you know where Sam went?" "I don't know where he went." You said.`,
+story:`You walk to Sam's family. "Are you okay ${name}? Sam was gone when we woke up. Do you know where Sam went?" asked his mother in a worried voice. "I don't know where he went." You said.`,
 image: "", 
 choices: [
 	{
@@ -445,7 +445,7 @@ choices: [
 },
 pg27: { /*From: pg24 | To: pg31 and pg32*/
 title: "Helping finding Sam", 
-story:`You helped finding Sam. What do you think Sam went?`,
+story:`You helped finding Sam. Where do you think Sam went?`,
 image: "", 
 choices: [
 	{
@@ -484,9 +484,9 @@ choices: [
 	}
 ]
 },
-pg30: { /*From: pg25 | To: pg35 and pg36*/
+pg30: { /*From: pg25 | To: pg35 and pg32mod1*/
 title: "Going outside", 
-story:`You go outside and Sams father said to you "Have you seen him?" you said "Nope".`,
+story:`You go outside and Sam's father said to you "Have you seen him?". You said "Nope".`,
 image: "", 
 choices: [
 	{
@@ -495,13 +495,297 @@ choices: [
 	},
 	{
 	choice: "Find Sam in other houses", 
-	destination: 'pg36'
+	destination: 'pg32mod1'
+	}
+]
+},
+pg31: { /*From: pg27 | To: pg37 and end*/
+title: "Finding Sam", 
+story:`You went to the street to find Sam. Where will you find him ${name}?`,
+image: "", 
+choices: [
+	{
+	choice: "Bridge",
+	destination: 'pg37'
+	},
+	{
+	choice: "Car", 
+	destination: 'end'
+	}
+]
+},
+pg32: { /*From: pg27 | To: pg38, pg39, pg40, and pg41*/
+title: "Going to the other house", 
+story:`You walk to other peoples house and you saw an old man. The old man said "Where do you think your going?" he said in a scary voice.`,
+image: "", 
+choices: [
+	{
+	choice: "Talk to him",
+	destination: 'pg38'
+	},
+	{
+	choice: "Run away from him", 
+	destination: 'pg39'
+	},
+	{
+	choice: "Knock him out", 
+	destination: 'pg40'
+	},
+	{
+	choice: "Ignore him", 
+	destination: 'pg41'
+	}
+]
+},
+pg33: { /*From: pg29 | To:  pg42 and pg43*/
+title: "The gun", 
+story:`You pick up the gun. What will you do?`,
+image: "", 
+choices: [
+	{
+	choice: "Go outside",
+	destination: 'pg42'
+	},
+	{
+	choice: "Check the ammo", 
+	destination: 'pg43'
+	}
+]
+},
+pg34: { /*From: pg34 | To: pg44 and end*/
+title: "The bat", 
+story:`You pick up the bat. What will you do?`,
+image: "", 
+choices: [
+	{
+	choice: "Go outside",
+	destination: 'pg44'
+	},
+	{
+	choice: "Fight the shadow", 
+	destination: 'end'
+	}
+]
+},
+pg35: { /*From: pg30 | To: end*/
+title: "Finding Sam in the street", 
+story:`You try to find Sam in the street but saw the shadow appeared in front of you.`,
+image: "", 
+choices: [
+	{
+	choice: "Run",
+	destination: 'end'
+	}
+]
+},
+pg32mod1: { /*From: pg27 | To: pg38, pg39, pg40, and pg41*/
+title: "Going to the other house", 
+story:`You walk to other peoples house and you saw an old man. The old man said "Where do you think your going?".`,
+image: "", 
+choices: [
+	{
+	choice: "Talk to him",
+	destination: 'pg38'
+	},
+	{
+	choice: "Run away from him", 
+	destination: 'pg39'
+	},
+	{
+	choice: "Knock him out", 
+	destination: 'pg40'
+	},
+	{
+	choice: "Ignore him", 
+	destination: 'pg41'
+	}
+]
+},
+pg37: { /*From: pg31 | To: pg45 and pg46*/
+title: "Mezalozon Bridge", 
+story:`You went to the bridge and find traffic. You saw the shadow behind you.`,
+image: "", 
+choices: [
+	{
+	choice: "Run away from the shadow",
+	destination: 'pg45'
+	},
+	{
+	choice: "Go inside a car", 
+	destination: 'pg46'
+	}
+]
+},
+pg38: { /*From: pg32 and pg32mod1 | To: pg47 and pg48*/
+title: "The old man", 
+story:`You talk to him. What will you say?`,
+image: "", 
+choices: [
+	{
+	choice: "Do you know where my friend Sam went?",
+	destination: 'pg47'
+	},
+	{
+	choice: "Are you okay?", 
+	destination: 'pg48'
+	}
+]
+},
+pg39: { /*From: pg32 and pg32mod1 | To: pg47 and pg38*/
+title: "The old man", 
+story:`You run away from the old man. Where will you go?`,
+image: "", 
+choices: [
+	{
+	choice: "Run",
+	destination: 'pg47'
+	},
+	{
+	choice: "Talk to the old man again", 
+	destination: 'pg38'
+	}
+]
+},
+pg40: { /*From: pg32 | To: end and end*/
+title: "Knocking the old man", 
+story:`You knocked the old man out by punching him in his face. `,
+image: "", 
+choices: [
+	{
+	choice: "Run",
+	destination: 'end'
+	},
+	{
+	choice: "Carry his body", 
+	destination: 'end'
+	}
+]
+},
+pg41: { /*From: pg32 | To: pg49*/
+title: "The old man", 
+story:`You ignored the old man. He said to you "You can't ignore me!" You look behind and saw him hitting you a stick and you were knock out.`,
+image: "", 
+choices: [
+	{
+	choice: "zzzzzzzzzzzzzzzz",
+	destination: 'pg49'
+	}
+]
+},
+pg42: { /*From: pg33 | To: end and end*/
+title: "The shadow", 
+story:`You went outside and saw the shadow.`,
+image: "", 
+choices: [
+	{
+	choice: "Shoot",
+	destination: 'end'
+	},
+	{
+	choice: "Run", 
+	destination: 'end'
+	}
+]
+},
+pg43: { /*From: pg33 | To: pg42*/
+title: "The gun", 
+story:`You check the ammo and it was full.`,
+image: "", 
+choices: [
+	{
+	choice: "Go outside",
+	destination: 'pg42'
+	}
+]
+},
+pg44: { /*From: pg34 | To: end and end*/
+title: "The shadow", 
+story:`You go outside and saw the shadow.`,
+image: "", 
+choices: [
+	{
+	choice: "Hit the Shadow",
+	destination: 'end'
+	},
+	{
+	choice: "Run", 
+	destination: 'end'
+	}
+]
+},
+pg45: { /*From: pg37 | To: end*/
+title: "The shadow", 
+story:`You run away from the shadow. The shadow was screeching.`,
+image: "", 
+choices: [
+	{
+	choice: "Run",
+	destination: 'end'
+	}
+]
+},
+pg46: { /*From: pg37 | To: end*/
+title: "The shadow", 
+story:`You went inside a car end waited until the shadow was gone. But you saw the shadow.`,
+image: "", 
+choices: [
+	{
+	choice: "Don't move",
+	destination: 'pg11'
+	}
+]
+},
+pg47: { /*From: pg38 and pg39 | To: pg49*/
+title: "The old man", 
+story:`"Do you know where Sam went?" you said. He knocked you with his walking stick.`,
+image: "", 
+choices: [
+	{
+	choice: "zzzzzzzzzzzzzzzz",
+	destination: 'pg49'
+	}
+]
+},
+pg48: { /*From: pg38 and pg39 | To: pg49*/
+title: "The old man", 
+story:`"Are you okay?" you said. He said "Always fine!" he knocked you out with his walking stick.`,
+image: "", 
+choices: [
+	{
+	choice: "zzzzzzzzzz",
+	destination: 'pg49'
+	}
+]
+},
+pg49: { /*From: pg41, pg47 and pg48 | To: pg50*/
+title: "Waking up", 
+story:`You woke up in a dark house.`,
+image: "", 
+choices: [
+	{
+	choice: "Continue",
+	destination: 'pg50'
+	}
+]
+},
+pg50: { /*From: pg49 | To: pg51 and pg52*/
+title: "The Dark House (Congratulations!!!!! You made it to the another quarter of the game!!!!)", 
+story:`This place was dark you can't see anything.`,
+image: "", 
+choices: [
+	{
+	choice: "Find something",
+	destination: 'pg51'
+	},
+	{
+	choice: "Just wait", 
+	destination: 'pg52'
 	}
 ]
 },
 }
 }
-
+	
 /*All the most complex functions. THIS IS IMPORTANT, don't delete!*/
 document.addEventListener('DOMContentLoaded', function() {
 var button = document.querySelector('#start-button')
